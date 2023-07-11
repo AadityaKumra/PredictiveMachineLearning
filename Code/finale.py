4df2884d7d7c5477f4c81be3c73bbb99ef1165f6
Aaditya Kumra <aadityakumra@gmail.com>
2023-07-11 15:36:02 +0530

    Update finale.py

--- Code/finale.py
+++ Code/finale.py
@@ -29,11 +29,11 @@ from catboost import CatBoostClassifier
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-import streamlit as st
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import streamlit as st
+# import streamlit as st
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 
 
 def page1():
